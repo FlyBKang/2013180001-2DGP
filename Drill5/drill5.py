f89dacc4958500d225121c9ac2e3c1ae24da6908
FlyBKang <cdcd8894@naver.com>
2018-09-17 16:22:46 +0900

    드릴 5 완성(Re)
    2013180001 강현웅
    https://youtu.be/H8QZw7onl8Y
    
    드릴 5에
    if(myX + myY == 0)를
    myX == myY ==0로 수정
    기존값 타입은 float형으로 int와 비교하는 경우 방지

--- Drill5/drill5.py
+++ Drill5/drill5.py
@@ -14,70 +14,75 @@ H = 800
 
 open_canvas(W, H)
 character = load_image('animation_sheet.png')
-arr = [203, 535, 132, 243, 535, 470,477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
-myX, myY,frame = 400,400,0
+arr = [203, 535, 132, 243, 535, 470, 477, 203, 715, 136, 316, 225, 510, 92, 692, 518, 682, 336, 712, 349]
+myX, myY, frame = 400, 400, 0
 
 
 def Move2Pos(x, y):  # 캐릭터 이동을 방향을 정의합니다.
     global myX, myY, frame
 
-    Col_X,Col_Y = 0,0
+    Col_X, Col_Y = 0, 0
+
     def MoveRight():
         global myX, myY
         myX = myX + 1
+
     def MoveLeft():
         global myX, myY
         myX = myX - 1
+
     def MoveUp():
         global myX, myY
         myY = myY + 1
+
     def MoveDown():
         global myX, myY
         myY = myY - 1
 
-    if(x - myX < 0):    #이전값과 비교
+    if (x - myX < 0):  # 이전값과 비교
         Col_X = -1
     else:
         Col_X = 1
-    if(y - myY < 0):
+    if (y - myY < 0):
         Col_Y = -1
     else:
         Col_Y = 1
+    print(Col_X, Col_Y)
 
     def SetDirection(n):
         global myX, myY, frame
         clear_canvas()
-        if(n>0):
-            character.clip_draw(frame * 100, 100, 100, 100, myX, myY)
+        if (n > 0):
+            character.clip_draw(frame * 100, 100, 90, 90, myX, myY)
         else:
-            character.clip_draw(frame * 100, 200, 100, 100, myX, myY)
-        update_canvas()
+            character.clip_draw(frame * 100, 0, 90, 90, myX, myY)
         frame = (frame + 1) % 8
+        update_canvas()
 
     while True:
-        if(Col_X>0):
+        if (Col_X > 0):
             MoveRight()
         elif (Col_X < 0):
             MoveLeft()
-
-        if(Col_Y>0):
+        if (Col_Y > 0):
             MoveUp()
-        elif(Col_Y<0):
+        elif (Col_Y < 0):
             MoveDown()
         SetDirection(Col_X)
 
-        if(myX -5 < x < myX+5):
+        if (myX - 3 < x < myX + 3):
             Col_X = 0
-        if(myY -5 < y < myY+5):
+        if (myY - 3 < y < myY + 3):
             Col_Y = 0
-        if(Col_X+Col_Y == 0):
+        if (Col_X == Col_Y == 0):
             break
 
 
 while (True):
-    for i in range(0,20,2):
-        Move2Pos(arr[i],arr[i+1])
-
+    for i in range(0, 20, 2):
+        print(i, arr[i], i + 1, arr[i + 1])
+        Move2Pos(arr[i], arr[i + 1])
+        delay(0.5)
 
 close_canvas()
 
